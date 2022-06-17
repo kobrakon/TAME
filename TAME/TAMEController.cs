e71b8b5b8b9c560a2382b1119b30f800965ff09c
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-06-17 16:58:32 -0400

    refactor

--- TAME/TAMEController.cs
+++ TAME/TAMEController.cs
@@ -10,51 +10,49 @@ namespace AmbientMusic
     {
         private static SoundPlayer soundPlayer; // yeah yeah stinky soundplayer what the fuck ever
         public static Timer timer;
-        public static bool isPlaying = false;
-        public static bool TimerRunning = false;
-        public static bool eventIsRunning = false;
+        public static bool isPlaying { get; set; } = false;
+        public static bool TimerRunning { get; set; } = false;
+        public static bool eventIsRunning { get; set; } = false;
 
         public static void AmbienceTimer()
         {
-            timer = new Timer(Random.Range(300000, 600000)); // pick random time between 5 to 10 minutes
-            timer.Start(); // start timer
+            timer = new Timer(Random.Range(300000, 600000));
+            timer.Start();
             TimerRunning = true;
 
             timer.Elapsed += (sender, e) => PlayMusicTime(); // okay so literally nothing uses the sender and e args but the timer is a little bitch and refuses to run the event without them
 
-            timer.AutoReset = true; // reset
+            timer.AutoReset = true;
 
             timer.Enabled = true;
         }
 
-        public static void PlayMusicTime() // plays music according to the time interval
+        public static void PlayBigHurtAudio()
         {
-            var ipbh = UpdateHandler.IsPlayerBigHurt(); // get player near death bool and declare it
+            SoundPlayer("\\BepInEx\\plugins\\TAME\\Music\\NDE");
 
-            if (!ipbh) // if player isn't near dead
+            isPlaying = true;
+            eventIsRunning = true;
+
+            soundPlayer.PlaySync();
+        }
+
+        public static void PlayMusicTime()
+        {
+            var ipbh = UpdateHandler.IsPlayerBigHurt();
+
+            if (!UpdateHandler.IsPlayerBigHurt())
             {
-                SoundPlayer("\\BepInEx\\plugins\\TAME\\Music\\Ambience"); // get normal ambience
+                SoundPlayer("\\BepInEx\\plugins\\TAME\\Music\\Ambience");
 
-                isPlaying = true; // let everyone know music is playing
+                isPlaying = true;
 
-                soundPlayer.PlaySync(); // play sync so the method waits until music is done playing
+                soundPlayer.PlaySync();
 
-                isPlaying = false; // let everyone know music's done
-                AmbienceTimer(); // reset timer
-            } else
-            { // if player near dead
-                 SoundPlayer("\\BepInEx\\plugins\\TAME\\Music\\NDE"); // get near death event music
-
-                 isPlaying = true;
-                 eventIsRunning = true;
-
-                 soundPlayer.PlaySync();
-
-                 isPlaying = false;
-                 eventIsRunning = false;
-
-                 AmbienceTimer();
+                isPlaying = false;
+                AmbienceTimer();
             }
+            return;
         }
 
         public static void ShutUp()
@@ -67,12 +65,12 @@ namespace AmbientMusic
 
         private static SoundPlayer SoundPlayer(string fullpath)
         {
-            var path = Directory.GetFiles(Directory.GetCurrentDirectory() + fullpath, "*.wav"); // get the directory and path then search for wav files
-            int choices = Random.Range(0, path.Length); // pick random file
+            var path = Directory.GetFiles(Directory.GetCurrentDirectory() + fullpath, "*.wav");
+            int choices = Random.Range(0, path.Length);
 
             soundPlayer = new SoundPlayer
             {
-                SoundLocation = path[choices] // set soundlocation to the selected random file in the requested directory
+                SoundLocation = path[choices]
             };
 
             return soundPlayer;
