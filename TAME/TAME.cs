126ed30d98a17f85552c0b162d12b1e8f69a6af3
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-08-15 13:18:40 -0400

    upload rewrite

--- TAME/TAME.cs
+++ TAME/TAME.cs
@@ -1,20 +1,24 @@
 using BepInEx;
 using UnityEngine;
+using BepInEx.Logging;
 
 namespace AmbientMusic
 {
     [BepInPlugin("com.kobrakon.tame", "TAME", "1.0.0")]
-    public class MusicEnginePlugin : BaseUnityPlugin
+    public class Plugin : BaseUnityPlugin
     {
         public GameObject Hook;
+        public static ManualLogSource logger;
 
         private void Awake()
         {
+            logger = Logger;
             Hook = new GameObject("TAME"); // create hook
             Hook.AddComponent<UpdateHandler>(); // attach mono script
             DontDestroyOnLoad(Hook); // add hook
 
             Logger.LogInfo($"Tarkov Ambient Music Engine (TAME): Initializing");
+            TAMEController.GetAudio();
         }
     }
 }
